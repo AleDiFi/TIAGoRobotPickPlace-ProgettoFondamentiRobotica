4a2a531ed171e33c1a003edabf8f3824e21ec2e7
Alessandro Di Filippo <59366570+AleDiFi@users.noreply.github.com>
2025-05-11 20:52:33 +0200

    Added My_bringup (launch file)

---
+++ my_bringup/launch/action_app.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(
+            package='tiago_control',
+            executable='aruco_detection',
+            #name='ArucoPoseEstimator',
+        ),
+        Node(
+            package='tiago_control',
+            executable='tiago_head_scan_publisher',
+            #name='TiagoHeadScanPublisher',
+        )
+    ])
