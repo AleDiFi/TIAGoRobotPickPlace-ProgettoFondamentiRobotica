d63653c468c6829ed46648a3b67d434457e6ec07
Alessandro Di Filippo <59366570+AleDiFi@users.noreply.github.com>
2025-05-14 11:12:54 +0200

    Updated launch file with torso and arm joints

--- my_bringup/launch/action_app.launch.py
+++ my_bringup/launch/action_app.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
 
@@ -13,5 +14,11 @@ def generate_launch_description():
             package='tiago_control',
             executable='tiago_head_scan',
             #name='TiagoHeadScanPublisher',
-        )
+        ),
+        ExecuteProcess( cmd = ['ros2','topic','pub', '/torso_controller/follow_joint_trajectory','trajectory_msgs/msg/JointTrajectory','{joint_names: ["torso_lift_joint"], points: [{positions: [0.35], time_from_start: {sec:10}}]}'
+                              ],
+                        cmd= ['ros2','topic','pub', '/arm_controller/follow_joint_trajectory','trajectory_msgs/msg/JointTrajectory','{joint_names: ["arm_1_joint", "arm_2_joint", "arm_3_joint" , "arm_4_joint", "arm_5_joint", "arm_6_joint", "arm_7_joint"], points: [{positions: [0.07, 0.1, -3.1, 1.36, 2.05, 0.01, -0.05], time_from_start: {sec:10}}]}'
+                              ],
+                              shell=True
+                              )
     ])
